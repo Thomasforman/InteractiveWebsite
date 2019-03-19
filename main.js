e4041e9a81445db69964ba59b7c0471a41ba7d4b
Thomas forman <tefphotography@gmail.com>
2019-03-19 10:41:55 -0700

    commit1

---
+++ main.js
@@ -0,0 +1,96 @@
+// // download jquery here: http://jquery.com
+//
+// $(document).ready(function() {
+//
+//   $("p").click(function() {
+//     $("p").css("color", "green");
+//     // $(this).css("color", "green");
+//   });
+//   $("h1").click(function(){
+//     $("div").css("background", "blue");
+//
+//
+//   });
+//   $("h2").click(function(){
+//     $("div").css("background", "yellow");
+//
+//
+//   });
+//
+//
+// });
+// $(document).ready(function() {
+//
+//   $("#p1").click(function() {
+//     $("#p2").show();
+//   });
+//
+//   $("#p2").click(function() {
+//     $("#p3").show();
+//   });
+//
+//   $("#p3").click(function() {
+//     $("#p4").show();
+//   });
+//
+//   $('#p4').click(function() {
+//     $('p').hide();
+//   })
+//
+// });
+
+$(document).ready(function() {
+
+  $("#YesButton1").click(function() {
+
+    // $("p").css("color", "green");
+    $("img").attr("src", "http://www.petwave.com/-/media/Images/Center/Care-and-Nutrition/Cat/Kittensv2/Kitten-3.ashx");
+    $("#YesButton1").hide();
+
+      $('p').show();
+
+
+  });
+  $("#YesButton1").click(function() {
+  $("YesButton2").show();
+  });
+
+  $("#NoButton1").click(function() {
+    // $("p").css("color", "green");
+    $("img").attr("src", "https://external-preview.redd.it/GgAplCa8KVQ0wKbjwVho-7ekKtOFSaguKYAWw3iZ3zM.jpg?auto=webp&s=50b728dfee4b36526cbe416ac258d905d81777ef");
+
+  });
+  if ($("YesButton1").click()) {
+
+
+  } else {
+
+  }
+
+});
+$("#YesButton2").click(function() {
+
+  // $("p").css("color", "green");
+  $("img").attr("src", "http://www.petwave.com/-/media/Images/Center/Care-and-Nutrition/Cat/Kittensv2/Kitten-3.ashx");
+});
+
+
+// $(document).ready(function() {
+//
+//   $("#p1").click(function() {
+//     $("#p2").show();
+//   });
+//
+//   $("#p2").click(function() {
+//     $("#p3").show();
+//   });
+//
+//   $("#p3").click(function() {
+//     $("#p4").show();
+//   });
+//
+//   $('#p4').click(function() {
+//     $('p').hide();
+//   })
+//
+// });
